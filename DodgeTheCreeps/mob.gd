f10d48e8c2be67901126a2e98de4dcdf2058d6e8
SigurdFraRG <sorenlarsen2006@gmail.com>
2025-01-30 13:17:06 +0100

    FIxed the main scene

--- DodgeTheCreeps/mob.gd
+++ DodgeTheCreeps/mob.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names("Walk", "Swim", "Fly")
+	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
 
 

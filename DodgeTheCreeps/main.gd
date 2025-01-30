bf72da2d218f32500d664008286c0297852bc6fa
SigurdFraRG <sorenlarsen2006@gmail.com>
2025-01-30 13:53:01 +0100

    HUD

--- DodgeTheCreeps/main.gd
+++ DodgeTheCreeps/main.gd
@@ -13,14 +13,18 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get ready to not touch kids")
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
@@ -50,6 +54,3 @@ func _on_mob_timer_timeout():
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
-
-func _ready():
-	new_game()
